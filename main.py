97de20952d8617aaa416c656ebdca2c946040eea
Squirtlecon <97619754+Squirtlecon@users.noreply.github.com>
2022-08-12 23:19:07 +0000

    done some stuff

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-s = "
-s1 = s[:len(s)//2]
-s2 = s[len(s)//2:]
-print(s1,s2)
+def check_if_symmetric(string):
+  s = string
+  s1 = s[:len(s)//2]
+  s2 = s[len(s)//2:]
+  t1 = s1 [::-1]
+  if t1== s2:
+    return True
+  if t1 != s2:
+    return False
+
+def convert_to_numbers(string):
+  
