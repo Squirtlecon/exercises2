83a566184450f8e9f8207abf154ac9c5f3d97043
Squirtlecon <97619754+Squirtlecon@users.noreply.github.com>
2022-08-13 06:30:42 +0000

    stuff

--- main.py
+++ main.py
@@ -16,48 +16,71 @@ def convert_to_numbers(string):
     if h == " ":
       i = word.index(" ")
       word = word[:i]+['0']+word[i+1:]
-  for h in word:
     if h == "a":
       i = word.index("a")
-    word = word[:i]+['1']+word[i+1:]
-  for h in word:
+      word = word[:i]+['1']+word[i+1:]
     if h == "b":
       i = word.index("b")
       word = word[:i]+['2']+word[i+1:]
-  for h in word:
     if h == "c":
       i = word.index("c")
       word = word[:i]+['3']+word[i+1:]
-
-
-  for h in word:
     if h == "d":
       i = word.index("d")
       word = word[:i]+['4']+word[i+1:]
-  for h in word:
     if h == "e":
       i = word.index("e")
       word = word[:i]+['5']+word[i+1:]
-  for h in word:
     if h == "f":
       i = word.index("f")
       word = word[:i]+['6']+word[i+1:]
-  for h in word:
     if h == "g":
       i = word.index("g")
       word = word[:i]+['7']+word[i+1:]
-  for h in word:
     if h == "h":
       i = word.index("h")
       word = word[:i]+['8']+word[i+1:]
-  for h in word:
     if h == "i":
       i = word.index("i")
       word = word[:i]+['9']+word[i+1:]
-  for h in word:
     if h == "j":
       i = word.index("j")
       word = word[:i]+['10']+word[i+1:]
+    if h == "k":
+      i = word.index("k")
+      word = word[:i]+['11']+word[i+1:]
+    if h == "l":
+      i = word.index("l")
+      word = word[:i]+['12']+word[i+1:]
+    if h == "m":
+      i = word.index("m")
+      word = word[:i]+['13']+word[i+1:]
+    if h == "n":
+      i = word.index("n")
+      word = word[:i]+['14']+word[i+1:]
+    if h == "O":
+      i = word.index("O")
+      word = word[:i]+['15']+word[i+1:]
+    if h == "p":
+      i = word.index("p")
+      word = word[:i]+['16']+word[i+1:]
+    if h == "q":
+      i = word.index("q")
+      word = word[:i]+['17']+word[i+1:]
+    if h == "r":
+      i = word.index("r")
+      word = word[:i]+['18']+word[i+1:]
+    if h == "s":
+      i = word.index("s")
+      word = word[:i]+['19']+word[i+1:]
+    if h == "t":
+      i = word.index("t")
+      word = word[:i]+['20']+word[i+1:]
+    if h == "u":
+      i = word.index("u")
+      word = word[:i]+['21']+word[i+1:]
+    if h == "v":
+      i = word.index("v")
+      word = word[:i]+['22']+word[i+1:]
   print(word)
-    
-convert_to_numbers("a b c d e f g h i j k l m n o p")
+convert_to_numbers("a b c d e f g h i j k l m n o p q r s t u v w x y z")
