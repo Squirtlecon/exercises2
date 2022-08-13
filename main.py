7cded6eb1239b6d6c5102c298a036911a8a0dda0
Squirtlecon <97619754+Squirtlecon@users.noreply.github.com>
2022-08-13 05:19:41 +0000

    stufff

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 def check_if_symmetric(string):
   s = string
-  s1 = s[:len(s)//2]
-  s2 = s[len(s)//2:]
-  t1 = s1 [::-1]
-  if t1== s2:
+  t1 = s[::-1]
+  if t1 == s:
+    print("true")
     return True
-  if t1 != s2:
+  else:
+    print("false")
     return False
+check_if_symmetric("1231")
 
 
-word = "abcd"
+word = "abc d"
 word = list(word)
 y= len(word)
 z = word.index
+
+for h in word:
+    if h == " ":
+      i = word.index(" ")
+      word = word[:i]+['0']+word[i+1:]
 for h in word:
     if h == "a":
       i = word.index("a")
     word = word[:i]+['1']+word[i+1:]
+  
+
+for h in word:
+    if h == "b":
+      i = word.index("b")
+      word = word[:i]+['2']+word[i+1:]
+
+for h in word:
+    if h == "c":
+      i = word.index("c")
+      word = word[:i]+['3']+word[i+1:]
 print(word)
