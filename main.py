932c1edcef105a93ee5f8a86ea5b96bbb5cc0d8c
Squirtlecon <97619754+Squirtlecon@users.noreply.github.com>
2022-08-14 02:47:53 +0000

    done w assigments

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def check_if_symmetric(string):
   else:
     print("false")
     return False
-check_if_symmetric("1231")
 
 def convert_to_numbers(string):
   word = string
@@ -95,4 +94,93 @@ def convert_to_numbers(string):
       i = word.index("z")
       word = word[:i]+['26']+word[i+1:]
   print(word)
-convert_to_numbers("a b c d e f g h i j k l m n o p q r s t u v w x y z")
+  
+def convert_to_letters(array):
+  word = array
+  for h in word:
+    if h == 0:
+      i = word.index(0)
+      word = word[:i]+[' ']+word[i+1:]
+    if h == 1:
+      i = word.index(1)
+      word = word[:i]+['a']+word[i+1:]
+    if h == 2:
+      i = word.index(2)
+      word = word[:i]+["b"]+word[i+1:]
+    if h == 3:
+      i = word.index(3)
+      word = word[:i]+['c']+word[i+1:]
+    if h == 4:
+      i = word.index(4)
+      word = word[:i]+['d']+word[i+1:]
+    if h == 5:
+      i = word.index(5)
+      word = word[:i]+['e']+word[i+1:]
+    if h == 6:
+      i = word.index(6)
+      word = word[:i]+['f']+word[i+1:]
+    if h == 7:
+      i = word.index(7)
+      word = word[:i]+['g']+word[i+1:]
+    if h == 8:
+      i = word.index(8)
+      word = word[:i]+['h']+word[i+1:]
+    if h == 9:
+      i = word.index(9)
+      word = word[:i]+['i']+word[i+1:]
+    if h == 10:
+      i = word.index(10)
+      word = word[:i]+['j']+word[i+1:]
+    if h == 11:
+      i = word.index(11)
+      word = word[:i]+['k']+word[i+1:]
+    if h == 12:
+      i = word.index(12)
+      word = word[:i]+["l"]+word[i+1:]
+    if h == 13:
+      i = word.index(13)
+      word = word[:i]+["m"]+word[i+1:]
+    if h == 14:
+      i = word.index(14)
+      word = word[:i]+["n"]+word[i+1:]
+    if h == 15:
+      i = word.index(15)
+      word = word[:i]+['o']+word[i+1:]
+    if h == 16:
+      i = word.index(16)
+      word = word[:i]+['p']+word[i+1:]
+    if h == 17:
+      i = word.index(17)
+      word = word[:i]+['q']+word[i+1:]
+    if h == 18:
+      i = word.index(18)
+      word = word[:i]+['r']+word[i+1:]
+    if h == 19:
+      i = word.index(19)
+      word = word[:i]+['s']+word[i+1:]
+    if h == 20:
+      i = word.index(20)
+      word = word[:i]+['t']+word[i+1:]
+    if h == 21:
+      i = word.index(21)
+      word = word[:i]+['u']+word[i+1:]
+    if h == 22:
+      i = word.index(22)
+      word = word[:i]+['v']+word[i+1:]
+    if h == 23:
+      i = word.index(23)
+      word = word[:i]+['w']+word[i+1:]
+    if h == 24:
+      i = word.index(24)
+      word = word[:i]+['x']+word[i+1:]
+    if h == 25:
+      i = word.index(25)
+      word = word[:i]+['y']+word[i+1:]
+    if h == 26:
+      i = word.index(26)
+      word = word[:i]+['z']+word[i+1:]
+  print(word)
+
+  
+
+convert_to_letters([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27])
